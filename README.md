149de7d9402d002b305b00adb2ae6e40ca9974e2
SAEED.PANAHI <panahi.projects@gmail.com>
2023-08-20 00:25:56 +0100

    first release: added scripts

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# Toc Toc Toe
+# Welcome to Tic Tac Toe
+
+This is a simple implementation of the classic game, written in pure JavaScript, HTML, and CSS. It's a fun project that I've created during my leisure time, and I hope you enjoy playing it.
+
+Currently, the game supports 2-player mode, where two players can take turns placing their marks on a 3x3 grid. The first player to get three in a row (horizontally, vertically, or diagonally) wins the game. If all squares are filled and no player has won, then the game ends in a draw.
+
+In the future, I plan to implement 1-player mode, where the computer will play against the human player. This will add an extra level of challenge and fun to the game.
+
+To play, simply click on a square to place your mark (X or O). 
+
+I hope you have fun playing Tic Tac Toe! Let me know if you encounter any issues or have suggestions for improvements.
+### Demo
+![Screenshot](https://github.com/panahi-projects/tic-toc-toe/blob/main/public/tic-toc-toe.png)
+
+### Getting Started
+
+1. Clone the repository: git clone https://github.com/panahi-projects/tic-toc-toe.git
+2. Navigate to the project directory: `cd tic-toc-toe`
+3. Install the packages: `npm install`
+4. Run the application: `npm run dev` or `npm start`
+5. Open your browser and navigate to http://localhost:3000 to see the application in action.
+
 
 #### Node version
 This project has been started with Node v18.16.1
--- index.js
+++ index.js
@@ -1,7 +1,137 @@
 'use strict';
-import { text } from './utils/index.js';
 
 (function () {
-    console.log('Web server is running');
-    console.log(text());
+    let currentTurn = 'x',
+        isGameOver = false;
+    let moves = [];
+    const NUMBER_OF_GAME_PIECES = 9;
+
+    const Playground_Init = (gamePiecesNum = 0) => {
+        const playground = document.querySelector('#playground');
+
+        for (let i = 1; i <= gamePiecesNum; i++) {
+            const section = document.createElement('section');
+            section.className = 'cell';
+            section.innerHTML = '';
+            section.dataset.number = i;
+            section.onclick = () => {
+                if (isGameOver) return;
+                const selected_piece = +section.getAttribute('data-number');
+
+                let moveStatus = makeMove(selected_piece);
+                if (moveStatus?.cell) {
+                    moves.push(moveStatus);
+                }
+                if (moves.length) {
+                    makeResetButton();
+                }
+                isGameOver = gameOver();
+                if (isGameOver) {
+                    makeWinnigPrompt();
+                }
+            };
+            playground.appendChild(section);
+        }
+    };
+    const makeWinnigPrompt = () => {
+        const playground = document.querySelector('#playground');
+
+        const winningPrompt = document.createElement('div');
+        winningPrompt.className = 'winner-prompt';
+        winningPrompt.innerHTML = 'You Win! 🎉';
+        playground.appendChild(winningPrompt);
+    };
+    const resetButtonHandler = () => {
+        if (moves.length === 0) return;
+        const playground = document.querySelector('#playground');
+        while (playground.firstChild) {
+            playground.removeChild(playground.lastChild);
+        }
+        const actions = document.querySelector('.actions');
+        actions.removeChild(actions.lastChild);
+        moves = [];
+        isGameOver = false;
+        currentTurn = 'x';
+
+        Playground_Init(NUMBER_OF_GAME_PIECES);
+    };
+    const makeMove = (cellNumber) => {
+        if (moves.findIndex((x) => x.cell === cellNumber) >= 0) {
+            return {};
+        }
+        const cellContent = document.createElement('span');
+        if (currentTurn === 'x') {
+            cellContent.className = 'cell-x';
+            cellContent.innerHTML = 'X';
+            let xCell = document.querySelector(`section.cell:nth-child(${cellNumber})`);
+            xCell.appendChild(cellContent);
+
+            currentTurn = 'o';
+            return { turn: 'x', cell: cellNumber };
+        } else {
+            cellContent.className = 'cell-o';
+            cellContent.innerHTML = 'O';
+            let xCell = document.querySelector(`section.cell:nth-child(${cellNumber})`);
+            xCell.appendChild(cellContent);
+
+            currentTurn = 'x';
+            return { turn: 'o', cell: cellNumber };
+        }
+    };
+    const extractMoves = (collection = [], turn = '') => {
+        return collection
+            .map((x) => {
+                if (x.turn === turn) {
+                    return x.cell;
+                }
+            })
+            .filter((x) => x);
+    };
+    const makeResetButton = () => {
+        const actionsDiv = document.querySelector('.actions');
+        if (actionsDiv.querySelector('button')) return;
+
+        const resetButton = document.createElement('button');
+        resetButton.className = 'btn btn-glow';
+        resetButton.id = 'reset';
+        resetButton.textContent = 'RESET';
+        resetButton.onclick = resetButtonHandler;
+        actionsDiv.appendChild(resetButton);
+    };
+    const gameOver = () => {
+        const winningCombinations = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+        ];
+
+        let xMoves = extractMoves(moves, 'x');
+        let oMoves = extractMoves(moves, 'o');
+
+        let hasWon = false;
+
+        for (let combination of winningCombinations) {
+            if (xMoves.includes(combination[0]) && xMoves.includes(combination[1]) && xMoves.includes(combination[2])) {
+                hasWon = true;
+                break;
+            }
+            if (oMoves.includes(combination[0]) && oMoves.includes(combination[1]) && oMoves.includes(combination[2])) {
+                hasWon = true;
+                break;
+            }
+        }
+        if (hasWon) {
+            console.log('You Win! 🎉');
+        }
+        return hasWon;
+    };
+    console.log('Web server is running!');
+
+    Playground_Init(NUMBER_OF_GAME_PIECES);
+    // resetButton();
 })();
